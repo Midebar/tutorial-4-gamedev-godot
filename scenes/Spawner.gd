b041f71413df9f78002218877cc1823a6ed501d3
Mikhael <midebar@gmail.com>
2024-03-06 22:45:38 +0700

    Added custom level

--- scenes/Spawner.gd
+++ scenes/Spawner.gd
@@ -1,8 +1,11 @@
 extends Node2D
 
 export (PackedScene) var obstacle
+export var start_cooldown = 5
+export var cooldown = 1
 
 func _ready():
+	yield(get_tree().create_timer(start_cooldown), "timeout")
 	repeat()
 
 func spawn():
@@ -10,11 +13,11 @@ func spawn():
 	get_parent().add_child(spawned)
 
 	var spawn_pos = global_position
-	spawn_pos.x = spawn_pos.x + rand_range(-250, 250)
+	spawn_pos.x = spawn_pos.x + rand_range(-25, 25)
 
 	spawned.global_position = spawn_pos
 
 func repeat():
 	spawn()
-	yield(get_tree().create_timer(1), "timeout")
+	yield(get_tree().create_timer(cooldown), "timeout")
 	repeat()
